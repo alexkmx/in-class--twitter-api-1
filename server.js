40c089cc3276327c4cd687c181d282c48c2d25af
Alejandro <alexk_mx@hotmail.com>
2017-11-21 22:08:09 -0600

    auth done

--- server.js
+++ server.js
@@ -8,6 +8,14 @@ const bodyParser = require('body-parser');
 
 const pageRouter = require('./src/routes/pageRouter.js');
 const apiRouter = require('./src/routes/apiRouter.js');
+const authRouter = require('./src/routes/authRouter.js');
+
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const cookieParser = require('cookie-parser');
+
+const registerLocalStrategy = require('./src/middleware/passport-local--registerLocalStrategy.js');
+const { configDeserializeUser, configSerializeUser } = require('./src/helpers/passport-local--sessionActions.js');
 
 const app = express();
 
@@ -24,10 +32,25 @@ app.set('views', `${__dirname}/src/views`);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+app.use(cookieSession({
+  name: 'cookiemonster',    //Aqui le paso el nombre de valor que quiera
+  secret: 'superdupersupersecret',
+  httpOnly: true,
+  signed: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(registerLocalStrategy());
+passport.serializeUser(configSerializeUser());
+passport.deserializeUser(configDeserializeUser());
+
 app.use(express.static(`${__dirname}/public`))
 
 app.use('/', pageRouter);
 app.use('/api/v1', apiRouter);
+app.use('/auth', authRouter);
 
 // Create 404 route
 app.use((req, res) => {
